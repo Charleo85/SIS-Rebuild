90d73af8d8531d6b6e2c6484564514e0146911da
tong qiu <tq7bw@virginia.edu>
2016-09-30 10:21:19 -0400

    dynamic webpage

--- web/web/views.py
+++ web/web/views.py
@@ -4,10 +4,16 @@ def home_page(request):
     return render(request, 'homepage.html')
 
 def all_courses(request):
-    return render(request, 'Course.html')
+    req = urllib.request.Request('http://exp-api:8000/course/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    return render(request, 'course.html', resp)
 
 def course_detail(request, sisid):
-    return render(request, 'course_detail.html')
+    req = urllib.request.Request('http://exp-api:8000/course/' + sisid + '/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    return render(request, 'course_detail.html', resp)
 
 def about(request):
     return render(request, 'about.html')
