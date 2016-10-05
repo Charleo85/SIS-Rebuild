8696067b571b7de1a6d304a229efd164f76614c1
Zakey Faieq <Zakey Faieq>
2016-10-05 14:00:22 -0400

    Finished Model Creation Unit Tests, added DuplicateIDTests, which currently do not raise Errorsgit status

--- models/api/tests.py
+++ models/api/tests.py
@@ -9,10 +9,10 @@ class ModelCreationTestCases(TestCase):
         instructor1.save()
         student1 = Student(first_name="Scott", last_name="Gilb", id="sg4fc")
         student1.save()
-        #course1 = Course(mnemonic="CS", number="3102", section="001", id=16947, instructor="gr3e", max_students=135)
-        #course1.save()
-        #enrollment1 = Enrollment(student="sg4fc", course=16947, enroll_status="E")
-        #enrollment1.save()
+        course1 = Course(mnemonic="CS", number="3102", section="001", id=16947, instructor=Instructor.objects.get(id="gr3e"), max_students=135)
+        course1.save()
+        enrollment1 = Enrollment(student=Student.objects.get(id="sg4fc"), course=Course.objects.get(id=16947), enroll_status="E")
+        enrollment1.save()
 
     def test_InstructorCreated(self):
         newInstructor = Instructor.objects.get(id="gr3e")
@@ -22,9 +22,14 @@ class ModelCreationTestCases(TestCase):
         newStudent = Student.objects.get(id="sg4fc")
         self.assertEqual(newStudent.__str__(), "Scott Gilb (sg4fc)")
 
-    #def test_CourseCreated(self):
+    def test_CourseCreated(self):
+        newCourse = Course.objects.get(id=16947)
+        self.assertEqual(newCourse.__str__(), "CS 3102")
 
-    #def test_EnrollmentCreated(self):
+    def test_EnrollmentCreated(self):
+        newEnrollment = Enrollment.objects.get(student="sg4fc")
+        self.assertEqual(newEnrollment.course, Course.objects.get(id=16947))
+
 """
 class ModelModificationTestCases(TestCase):
     fixtures = ['data.json']
@@ -37,15 +42,35 @@ class ModelModificationTestCases(TestCase):
 
     def test_modifyCourse(self):
 
+"""
 class TestCasesToInsureDuplicateIDsAreNotAllowed(TestCase):
     fixtures = ['data.json']
 
+    def setUp(self):
+        instructor1 = Instructor(first_name="Gabriel", last_name="Robins", id="gr3e")
+        instructor1.save()
+        student1 = Student(first_name="Scott", last_name="Gilb", id="sg4fc")
+        student1.save()
+        course1 = Course(mnemonic="CS", number="3102", section="001", id=16947, instructor=Instructor.objects.get(id="gr3e"), max_students=135)
+        course1.save()
+        enrollment1 = Enrollment(student=Student.objects.get(id="sg4fc"), course=Course.objects.get(id=16947), enroll_status="E")
+        enrollment1.save()
+
     def test_DuplicateStudentCheck(self):
+        studentDuplicate = Student.objects.get(id="sg4fc")
+        studentDuplicate.id = "zaf2xk"
+        studentDuplicate.save()
 
     def test_DuplicateInstructorCheck(self):
+        instructorDuplicate = Instructor.objects.get(id="gr3e")
+        instructorDuplicate.id = "tp3ks"
+        instructorDuplicate.save()
 
     def test_DuplicateCourseCheck(self):
-
+        courseDuplicate = Course.objects.get(id=16947)
+        courseDuplicate.id = 17894
+        courseDuplicate.save()
+"""
 class TestCasesToInsureAllForeignKeyFieldDataIsInDatabase(TestCase):
     fixtures = ['data.json']
 
