84f4aa5d4b327d51b939a1ca37754fc0c99cfb7e
tong qiu <tq7bw@virginia.edu>
2016-10-12 12:32:44 -0400

    add file

---
+++ web/web/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username: ', max_length=24)
+    password = forms.CharField(label='Password: ', max_length=100)
+
+    def is_valid(self):
+        valid = super(LoginForm, self).is_valid()
+        if not valid:
+            return False
+
+        pw = self.cleaned_data['password']
+        result1 = len(pw) >= 6
+        result2 = False
+        try:
+            pw = int(pw)
+        except ValueError:
+            result2 = True
+        return result1 and result2
