d13d33f3e8723d86760e31f516c2956aa7c1f86e
tong qiu <tq7bw@virginia.edu>
2016-11-09 19:14:29 -0500

    test of adding unittest

--- exp/exp/tests.py
+++ exp/exp/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+import json
 
-# Create your tests here.
+from . import views_model, views_auth
+
+
+class SearchTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_general_search(self):
+        post_data = {
+            'search_query': 'tp3ks',
+            'query_specifier': 'general',
+        }
+        request = self.factory.post('/search/', data=post_data)
+        response = views_model.search(request)
+        resp_data = json.loads(response.context, safe=False)
+
+        self.assertEqual(resp_data['status_code'], 200)
