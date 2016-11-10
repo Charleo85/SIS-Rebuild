8a74dfba71bcfe8c4de1c0212db217bda1f33022
tong qiu <tq7bw@virginia.edu>
2016-11-09 20:01:35 -0500

    fix bugs

--- exp/exp/tests.py
+++ exp/exp/tests.py
@@ -104,15 +104,15 @@ class CreateAndSearchTestCase(TestCase):
         request = self.factory.post('/student/signup/')
         views_auth.signup(request, 1)
 
-    def tearDown(self):
-        # delete both instances for testing
-        url = 'http://models-api:8000/api/course/delete/'
-        post_data = {'id': '20529'}
-        views_model._make_post_request(url, post_data)
-
-        url = 'http://models-api:8000/api/student/delete/'
-        post_data = {'id': 'yz9fy'}
-        views_model._make_post_request(url, post_data)
+    # def tearDown(self):
+    #     # delete both instances for testing
+    #     url = 'http://models-api:8000/api/course/delete/'
+    #     post_data = {'id': '20529'}
+    #     views_model._make_post_request(url, post_data)
+    #
+    #     url = 'http://models-api:8000/api/student/delete/'
+    #     post_data = {'id': 'yz9fy'}
+    #     views_model._make_post_request(url, post_data)
 
     def test_course_create(self):
         post_data = {
