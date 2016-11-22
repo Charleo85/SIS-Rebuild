c0dac85289a72a73afcc063cd0fe6563bba06686
tonyqiu1019 <tq7bw@virginia.edu>
2016-11-22 16:05:54 -0500

    fix bugs in shell script

--- cleardb.sh
+++ cleardb.sh
@@ -2,24 +2,16 @@
 
 set -e
 
-docker start mysql &> /dev/null || {
-    echo "Error starting mysql"
-    exit 1
-}
-
-docker start mysql-cmdline &> /dev/null || {
-    echo "Error entering mysql-cmdline!"
+docker start mysql mysql-cmdline &> /dev/null || {
+    echo "Error starting docker container(s)!"
     exit 1
 }
 sleep 1
 
 docker exec -it mysql-cmdline bash -c \
-    "echo \"mysql -uroot -p'\\\$3cureUS' -h db -Bse \\\"drop database cs4501;
-    create database cs4501 character set utf8;
-    grant all on *.* to 'www'@'%'; \\\" \" > clear.sh;
-    chmod 755 clear.sh;
-    ./clear.sh;
-    rm -f clear.sh; " &> /dev/null || {
+"mysql -uroot -p'\$3cureUS' -h db -Bse \"drop database cs4501;
+create database cs4501 character set utf8;
+grant all on *.* to 'www'@'%'; \"; " &> /dev/null || {
     echo "Error clearing the database!"
     exit 2
 }
