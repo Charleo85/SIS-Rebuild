415d9fd9b58b78572644866ba7f44ae7f4a0b7bb
Charlie <wujibang@gmail.com>
2016-11-29 21:57:14 -0500

    Fixing test

--- MySQLSetup.sh
+++ MySQLSetup.sh
@@ -10,7 +10,7 @@ if [[ -e /app/db/cs4501 ]] ; then
     ";
 else
     mysql -uroot -p'\$3cureUS' -h mysql -Bse \
-    "create user 'www'@'%' identified by '$3cureUS';
+    "create user 'www'@'%' identified by '\$3cureUS';
     create database cs4501 character set utf8;
     grant all on *.* to 'www'@'%'; \
     ";
