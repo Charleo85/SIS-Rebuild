e22015f9d6116a17e145d135e851c9f62fae36be
tonyqiu1019 <tq7bw@virginia.edu>
2016-11-30 00:10:47 -0500

    should be working

--- MySQLSetup.sh
+++ MySQLSetup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sleep 60
+sleep 30
 
 if [[ -e /app/db/cs4501 ]] ; then
-    mysql -uroot -p'\$3cureUS' -h db -Bse \
+    mysql -uroot -p'$3cureUS' -h db -Bse \
     "drop database cs4501;
     create database cs4501 character set utf8;
     grant all on *.* to 'www'@'%'; \
     ";
 else
-    mysql -uroot -p'\$3cureUS' -h db -Bse \
+    mysql -uroot -p'$3cureUS' -h db -Bse \
     "create user 'www'@'%' identified by '\$3cureUS';
     create database cs4501 character set utf8;
     grant all on *.* to 'www'@'%'; \
